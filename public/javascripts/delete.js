de7214a81be49f113ded908028225a0ff45f3c6b
Peter-S <32533746+Peter-S@users.noreply.github.com>
2019-03-14 19:14:11 -0700

    Add files via upload

---
+++ public/javascripts/delete.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  //variable
+  var myVar = document.getElementById('del');
+  
+  //call back function
+  myVar.addEventListener('click', function() {
+    alert("help");
+  });
+}
